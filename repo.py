4dcfc64f5c387649ae5cc450e2ceb28f8f3dd6c9
grold0123 <172453552+grold0123@users.noreply.github.com>
2025-10-27 08:48:00 +0800

    update

---
+++ repo.py
@@ -0,0 +1,22 @@
+import subprocess 
+
+base_dir = "C:\projects\code_it_yourself"
+
+args = [
+    ('\n\ngit add:\n\n',[
+        'git','add',"C:\projects\code_it_yourself"
+    ]),#git add
+    ('\n\ngit commit:\n\n',[
+        'git','commit','-m',"update"
+    ]),#git commit
+    ('git push:\n\n',[
+        'git','push'
+    ]),#git push
+]
+
+
+for prompt,arg in args:
+    subprocess.run(arg)
+    print("successful")
+
+print("\n\nRepository updated\n\n")
